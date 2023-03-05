3f9a4f02fa838f9567a1fd4380d728d9442637ab
chw8207 <chw8207@naver.com>
2023-03-05 21:28:05 +0900

    구구단 프로그램 작성중2

--- 구구단 프로그램.py
+++ 구구단 프로그램.py
@@ -4,4 +4,9 @@ import time, random
 def printAllTimeTable(time) :  # 주어진 숫자부터 9단까지 출력
     for start in range(time, 10) : 
         for multiply in range(10) : 
-            print('%d x %d = %d' % (time, i, time*i))
+            print('%d x %d = %d' % (time, start, time*start))
+
+def printTimeTable(time) :    # 주어진 숫자의 단만 출력
+    for i in range(10) : 
+        print('%d x %d = %d' % (time, i, time*i))
+
